0c99faa23096fdb82096c9c0442f726100f47ac4
WinnieDaDarkDrag <71850362+WinnieDaDarkDrag@users.noreply.github.com>
2022-03-09 12:55:36 -0800

    airtable form thing

---
+++ components/navbar/Navbar.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import css from 'styled-jsx/css'
+import Link from 'next/link'
+import {FaGithub} from 'react-icons/fa'
+import {SiReplit} from 'react-icons/si'
+
+function navbar() {
+  return (
+    <>
+    <style jsx>{styles}</style>
+    <nav>
+        <div className="logo"></div>
+        <div className="links">
+          <Link href="/">
+            <a>Projects</a>
+          </Link>
+
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </div>
+        <div className="social">
+          <a href="http://github.com">
+            <FaGithub />
+          </a>
+          <a href="http:replit.com">
+            <SiReplit />
+          </a>
+        </div>
+    </nav>
+  </>
+  )
+}
+
+const styles = css`
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 20px;
+    padding: 20px;
+}
+.logo {
+  margin-right: auto;
+}
+.links {
+  display: flex;
+  margin-right: 40px;
+}
+a {
+  padding-left: 10px;
+  padding-right: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+  transition: all 0.2s;
+  vertical-align: middle;
+  color: rgba(255, 255, 255, 0.8);
+}
+a:hover {
+  color: rgba(255, 255, 255, 1);
+  transform: scale(1.04);
+}
+.social a {
+  font-size: 28px;
+}`
+
+
+export default navbar;
---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout/Layout';
+import Navbar from '../components/navbar/Navbar';
+
+function about() {
+  return
+    <Layout>
+
+    <h1>sup im the about</h1>
+    <p>balls</p>
+    </Layout>;
+}
+
+export default about;
